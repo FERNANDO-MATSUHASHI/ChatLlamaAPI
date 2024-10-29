2b0b1521c316deb5e18fde54abde27171db81f59
FERNANDO-MATSUHASHI <matsu.zf@gmail.com>
2024-10-29 01:25:33 -0300

    Correção Port

--- llamaAPI.py
+++ llamaAPI.py
@@ -120,4 +120,4 @@ def chat():
 # Executa a aplicação Flask
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(host=hostRender, port=portRender, debug=True)
+    app.run(host=hostRender, port=portRender)
